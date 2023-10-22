c781e25a4ad18b3be6c10dd600685a939a76e489
programador92 <edwinprogramacion@hotmail.es>
2023-10-22 02:32:10 -0500

    test

--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
   public static void main(String[] args) {
+	  System.out.println("Init"); 
     SpringApplication.run(Main.class, args);
   }
 }
